27433f432a9ec10b28f9f3aee571e725b2e2cd55
CharlesSigo <chuck@schooldatebooks.com>
2018-03-21 10:37:07 -0400

    implemented navigation router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
 import 'bootstrap';
+//import 'adal';
+import {RouterConfiguration, Router} from 'aurelia-router';
 
 export class App {
-  message = 'Hello World!';
+    router: Router;
+
+    constructor() {
+        //let authContext = new AuthenticationContext({ clientId: 'asdf' });
+    }
+
+    configureRouter(config: RouterConfiguration, router: Router): void {
+        this.router = router;
+        config.title = 'Aurelia';
+        config.options.root = '/';
+        config.options.pushState = true;
+        config.map([
+            { route: ['login'], name: 'login',  moduleId: 'resources/views/login/index' },
+            { route: ['', 'home'], name: 'home',  moduleId: 'resources/views/home/index', nav: true, title: "Home" },
+            { route: ['reports'], name: 'reports',  moduleId: 'resources/views/reports/index', nav: true, title: "Reports" }
+        ]);
+    }
 }
---
+++ src/resources/views/home/index.ts
@@ -0,0 +1,3 @@
+export class Home {
+    viewName = "Home"
+}
